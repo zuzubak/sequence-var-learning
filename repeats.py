5294511eb137b45932eb86071b486fb89d413a48
Malcolm Kennedy <zuzubak@gmail.com>
2019-09-16 11:06:52 -0400

    add notes to repeat

--- repeats.py
+++ repeats.py
@@ -1,6 +1,10 @@
 import index
 
 def repeats(fp_or_songs_str,syllable,max_count=5):
+    '''Given either a songstrings file, or a list of song strings, and a specific syllable, 
+    returns a dictionary of how many instances there are of the syllable being repeated n times, 
+    from n=1 to n=max_count.
+    '''
     count=0
     counts_dict={}
     for i in range(1,max_count+1):
