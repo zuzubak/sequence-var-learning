1dcd563beab1bb768eb9e8e152906a4b413f742e
Malcolm Kennedy <zuzubak@gmail.com>
2020-01-05 14:30:34 -0500

    umap

---
+++ umap_commands.py
@@ -0,0 +1,55 @@
+import scikit
+from sklearn.datasets import fetch_mldata
+from sklearn.decomposition import PCA
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+# Dimension reduction and clustering libraries
+import umap
+import hdbscan
+import sklearn.cluster as cluster
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
+
+sns.set(style='white', rc={'figure.figsize':(10,8)})
+mnist = fetch_mldata('MNIST Original')
+
+standard_embedding = umap.UMAP(random_state=42).fit_transform(mnist.data)
+plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], c=mnist.target, s=0.1, cmap='Spectral');
+
+kmeans_labels = cluster.KMeans(n_clusters=10).fit_predict(mnist.data)
+
+plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], c=kmeans_labels, s=0.1, cmap='Spectral');
+
+(
+    adjusted_rand_score(mnist.target, kmeans_labels),
+    adjusted_mutual_info_score(mnist.target, kmeans_labels)
+)
+
+(0.36675295135972552, 0.49614118437750965)
+
+lowd_mnist = PCA(n_components=50).fit_transform(mnist.data)
+hdbscan_labels = hdbscan.HDBSCAN(min_samples=10, min_cluster_size=500).fit_predict(lowd_mnist)
+
+clustered = (hdbscan_labels >= 0)
+plt.scatter(standard_embedding[~clustered, 0],
+            standard_embedding[~clustered, 1],
+            c=(0.5, 0.5, 0.5),
+            s=0.1,
+            alpha=0.5)
+plt.scatter(standard_embedding[clustered, 0],
+            standard_embedding[clustered, 1],
+            c=hdbscan_labels[clustered],
+            s=0.1,
+            cmap='Spectral');
+
+adjusted_rand_score(mnist.target, labels), adjusted_mutual_info_score(mnist.target, labels)
+
+clustered = (labels >= 0)
+(
+    adjusted_rand_score(mnist.target[clustered], labels[clustered]),
+    adjusted_mutual_info_score(mnist.target[clustered], labels[clustered])
+)
+
+np.sum(clustered) / mnist.data.shape[0]
