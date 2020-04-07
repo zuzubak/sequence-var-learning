0190796484f737b860268194049e466590ba0f4e
Malcolm Kennedy <zuzubak@gmail.com>
2020-04-07 13:38:50 -0400

    Added Dave Mets\' song DKL measurement, slightly modified from journals.plos.org/ploscompbiol/article/file?type=supplementary&id=info:doi/10.1371/journal.pcbi.1006437.s006

--- syllabify.py
+++ syllabify.py
@@ -2,6 +2,7 @@ import evfuncs
 from pydub import AudioSegment
 import string
 import random
+import os
 
 def get_syls(bird_directory,target_syllable,prepad=10,postpad=10,shuffle=False,n=50, notbatch='notbatch'):
     notmat_lines_list = list(open(bird_directory+notbatch))
@@ -9,21 +10,29 @@ def get_syls(bird_directory,target_syllable,prepad=10,postpad=10,shuffle=False,n
     if shuffle==True:
         random.shuffle(notmat_list)
     f=0
-    syllable_directory = bird_directory+'/'+target_syllable+'/'
+    if not os.path.exists(bird_directory+'/syllables/'):
+        os.mkdir(bird_directory+'/syllables/')
+    syllable_directory = bird_directory+'/syllables/'+target_syllable+'/'
     if not os.path.exists(syllable_directory):
         os.mkdir(syllable_directory)
     for file in notmat_list:
         ndict = evfuncs.load_notmat(file)
         wav_file = file.strip('.not.mat')
-        file_audio = AudioSegment.from_wav(wav_file)
-        i=0
-        for syl in ndict['labels']:
-            if syl == target_syllable and spectrograms_created < n:
-                onset=ndict['onsets'][i]
-                offset=ndict['offsets'][i]
-                syl_snippet = file_audio[onset-prepad:offset+postpad]
-                ID = str(f)+'_'+str(i)
-                snippet_filename = syllable_directory+'/'+ID+'.wav'
-                syl_snippet.export(snippet_filename,format='wav')
-                i+=1
-        f+=1
+        try:
+            file_audio = AudioSegment.from_wav(wav_file)
+            i=0
+            for syl in ndict['labels']:
+                if syl == target_syllable:
+                    try:
+                        onset=ndict['onsets'][i]
+                        offset=ndict['offsets'][i]
+                        syl_snippet = file_audio[onset-prepad:offset+postpad]
+                        ID = str(f)+'_'+str(i)
+                        snippet_filename = syllable_directory+'/'+ID+'.wav'
+                        syl_snippet.export(snippet_filename,format='wav')
+                        i+=1
+                    except:
+                        pass
+            f+=1
+        except:
+            pass
