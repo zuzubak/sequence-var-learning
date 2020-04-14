363153c34b18c9bd67d91295b5ca3f7f0c170e8d
Malcolm Kennedy <zuzubak@gmail.com>
2020-04-14 18:03:54 -0400

    added make_pptx() function to spectrograms.py, to create powerpoint slides of spectrograms for classification purposes

--- spectrograms.py
+++ spectrograms.py
@@ -7,6 +7,10 @@ from pydub import AudioSegment
 import string
 import random
 from scipy import signal
+from pptx import Presentation
+from pptx.util import Inches
+import os
+
 
 ###modified from https://stackoverflow.com/questions/47147146/save-an-image-only-content-without-axes-or-anything-else-to-a-file-using-matl
 def graph_spectrogram(wav_file,directory,ID='no_ID'):
@@ -15,8 +19,9 @@ def graph_spectrogram(wav_file,directory,ID='no_ID'):
     fig.patch.set_visible(False) #get rid of white space around spectrograms -- from https://stackoverflow.com/questions/14908576/how-to-remove-frame-from-matplotlib-pyplot-figure-vs-matplotlib-figure-frame
     ax.axis('off') #turn off x (time) and y (frequency) axes
     pxx, freqs, bins, im = ax.specgram(x=data, Fs=rate, noverlap=511, NFFT=512)
-    fig.set_size_inches(len(data)/3000,4) #scale the figure by the length (in time) of the audio sample being plotted, so that output images for longer samples are wider.
-    plt.savefig(directory+'spectrograms/'+ID+'.png', dpi=300, frameon=False)
+    ax.set_ylim([0,10000])
+    fig.set_size_inches(len(data)/8000,1.5) #scale the figure by the length (in time) of the audio sample being plotted, so that output images for longer samples are wider.
+    plt.savefig(directory+'spectrograms/'+ID+'.png', dpi=400, frameon=False)
 
 def graph_spectrogram2(wav_file,directory,ID='no_ID'):
     fs, x = wavfile.read(wav_file)
@@ -24,7 +29,11 @@ def graph_spectrogram2(wav_file,directory,ID='no_ID'):
     plt.pcolormesh(t, f[0:20], Sxx[0:20])
     plt.show()
 
-def get_syls(directory,prepad=10,postpad=10,shuffle=True,n=50, notbatch='notbatch'):
+def get_syls(directory,prepad=20,postpad=20,shuffle=True,n=50, notbatch='notbatch'):
+    if not os.path.exists(directory+'snippets'):
+        os.makedirs(directory+'snippets')
+    if not os.path.exists(directory+'spectrograms'):
+        os.makedirs(directory+'spectrograms')
     notmat_lines_list = list(open(directory+notbatch)) #extract a list of labelled songfiles
     notmat_list = [directory+value[:value.index('\n')] for value in notmat_lines_list] #extract a list of labelled songfiles
     if shuffle==True: #randomize the order in which the files are processed, if the parameter "shuffle" is set to True
@@ -35,18 +44,56 @@ def get_syls(directory,prepad=10,postpad=10,shuffle=True,n=50, notbatch='notbatc
         ndict = evfuncs.load_notmat(file) #loads variables stored in the notmat file pertaining to the current songfile
         wav_file = file.strip('.not.mat') #the wav file corresponding to the current notmat file should be the same, minus the ".not.mat" at the end
         file_audio = AudioSegment.from_wav(wav_file) #create an AudioSegment object of the relevant wave file, so that it can be sliced up.
-        i=0 #keeps track of how many spectrograms have been generated for the current songfile
+        i=1 #keeps track of how many spectrograms have been generated for the current songfile
         for syl in ndict['labels']: #iterate through the labelled syllable tokens in the songfile
             if syl in string.ascii_letters and spectrograms_created < n: #check that the current syllable is a letter of the alphabet (so that "-" and "0" will be ignored)
-                onset=ndict['onsets'][i] #retrieve the onset time (in milliseconds) of the syllable within the file
-                offset=ndict['offsets'][i]
-                syl_snippet = file_audio[onset-prepad:offset+postpad] #create an AudioSegment that begins "prepad" ms before the syllable begins, and ends "postpad" milliseconds after it ends, where those two parameters are specified by the user.
-                ID = str(f)+'_'+str(i) #create an arbitrary ID for the current syllable, by concatenating the number of songs that have been processed and the number of syllables processed in the current song, with an underscore in between.
-                snippet_filename = directory+'snippets/'+ID+'.wav'
-                syl_snippet.export(snippet_filename,format='wav')
-                graph_spectrogram(snippet_filename,directory,ID=ID)
-                i+=1
-                spectrograms_created += 1
+                try:
+                    onset=ndict['onsets'][i] #retrieve the onset time (in milliseconds) of the syllable within the file
+                    offset=ndict['offsets'][i]
+                    syl_snippet = file_audio[onset-prepad:offset+postpad] #create an AudioSegment that begins "prepad" ms before the syllable begins, and ends "postpad" milliseconds after it ends, where those two parameters are specified by the user.
+                    ID = str(f)+'_'+str(i) #create an arbitrary ID for the current syllable, by concatenating the number of songs that have been processed and the number of syllables processed in the current song, with an underscore in between.
+                    snippet_filename = directory+'snippets/'+ID+'.wav'
+                    syl_snippet.export(snippet_filename,format='wav')
+                    graph_spectrogram(snippet_filename,directory,ID=ID)
+                    i+=1
+                    spectrograms_created += 1
+                except BaseException:
+                    pass
         f+=1
 
         
+def make_pptx(directory,fname='for_categorization.pptx', border_width_inches=0.01, buffer_width_inches=0.01):
+    prs = Presentation()
+    title_slide_layout = prs.slide_layouts[6] #blank slide
+    slide = prs.slides.add_slide(title_slide_layout)
+    shapes = prs.slides[0].shapes
+    left = top = Inches(border_width_inches)
+    spectrograms_dir = directory + 'spectrograms/'
+    files_list = os.listdir(spectrograms_dir)
+    i=0
+    for spectrogram in files_list:
+        spec_fp = spectrograms_dir + spectrogram
+        if 'png' in spec_fp:
+            try:
+                picture = shapes.add_picture(spec_fp, left, top)
+                image_width = Inches(picture.image.size[0]/picture.image.dpi[0])
+                image_height = Inches(picture.image.size[1]/picture.image.dpi[1])
+                #left += Inches(buffer_width_inches)
+                if left + 2*image_width > prs.slide_width:
+                    if top + image_height < prs.slide_height: #Create new line if adding image to current line will exceed slide borders.
+                        left = Inches(border_width_inches)
+                        top += image_height
+                    else:
+                        break
+                else:
+                    left += image_width
+                i+=1
+            except BaseException:
+                pass
+    prs.save(directory+fname)
+
+def batch_slides(directory):
+    for bird_directory in os.listdir(directory):
+        if '.' not in bird_directory:
+            get_syls(directory+bird_directory+'/')
+            make_pptx(directory+bird_directory+'/')
